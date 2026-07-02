be50ac2ce5b4967393ae9de6353e7caf0557d3d9
Prabhakaran.D <prabhakaran.h.net@gmail.com>
2026-07-02 11:45:41 +0530

    Initial commit

--- script.js
+++ script.js
@@ -14,7 +14,7 @@
  */
 
 const EMAILJS_PUBLIC_KEY = 'pqI8vUzWYHZV5fppp';    // Account → API Keys
-const EMAILJS_SERVICE_ID = 'service_uk74pa6';    // Email Services → Service ID
+const EMAILJS_SERVICE_ID = 'service_7ad76by';    // Email Services → Service ID
 const EMAILJS_TEMPLATE_ID = 'template_kri320w';   // Email Templates → Template ID
 
 /* ─── 1. Initialize EmailJS ─────────────────────── */
